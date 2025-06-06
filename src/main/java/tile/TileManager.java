77019f44208c784b2bb32f68a0e958617fb20dfc
Yew12347 <98322702+Yew12347@users.noreply.github.com>
2025-06-06 15:41:52 +0700

    change tile a bit

--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -20,7 +20,7 @@ public class TileManager {
     public TileManager(GamePanel gp) {
 
         this.gp = gp;
-        tile = new Tile[10];
+        tile = new Tile[50];
         mapTileNum = new int[gp.maxWorldRow][gp.maxWorldRow];
 
         getTileImage();
@@ -37,12 +37,59 @@ public class TileManager {
 
     public void getTileImage () {
 
-        setup(0, "grass01", false);
-        setup(1, "wall", true);
-        setup(2, "water00", true);
-        setup(3, "earth", false);
-        setup(4, "tree", true);
-        setup(5, "road00", false);
+        setup(0, "grass00", false);
+        setup(1, "grass00", true);
+        setup(2, "grass00", true);
+        setup(3, "grass00", false);
+        setup(4, "grass00", true);
+        setup(5, "grass00", false);
+        setup(6, "grass00", false);
+        setup(7, "grass00", false);
+        setup(8, "grass00", false);
+        setup(9, "grass00", false);
+        setup(10, "grass00", false);
+        setup(11, "grass01", false);
+        setup(12, "water00", true);
+        setup(13, "water01", true);
+        setup(14, "water01", false);
+        setup(15, "water03", true);
+        setup(16, "grass00", false);
+        setup(17, "grass00", false);
+        setup(18, "grass00", false);
+        setup(19, "grass00", false);
+        setup(20, "grass00", false);
+        setup(21, "grass00", false);
+        setup(22, "grass00", false);
+        setup(23, "grass00", true);
+        setup(24, "grass00", true);
+        setup(25, "grass00", false);
+        setup(26, "grass00", false);
+        setup(27, "grass00", true);
+        setup(28, "grass00", false);
+        setup(29, "grass00", false);
+        setup(30, "grass00", false);
+        setup(31, "grass00", true);
+        setup(32, "grass00", false);
+        setup(33, "grass00", false);
+        setup(34, "grass00", false);
+        setup(35, "grass00", false);
+        setup(36, "grass00", true);
+        setup(37, "grass00", false);
+        setup(38, "grass00", false);
+        setup(39, "grass00", true);
+        setup(40, "grass00", false);
+        setup(41, "grass00", false);
+        setup(42, "grass00", true);
+        setup(43, "grass00", false);
+        setup(44, "grass00", false);
+        setup(45, "grass00", false);
+        setup(46, "grass00", false);
+        setup(47, "grass00", true);
+        setup(48, "grass00", false);
+        setup(49, "grass00", false);
+        setup(50, "grass00", false);
+        setup(51, "grass00", false);
+
     }
     public void setup(int index, String imagePath, boolean collision) {
 
